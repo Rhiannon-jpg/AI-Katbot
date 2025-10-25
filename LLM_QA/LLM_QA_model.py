ddf5f7315e7321a9703bc04741f5474719c75512
Rhiannon-jpg <60527709+Rhiannon-jpg@users.noreply.github.com>
2025-10-25 14:35:36 -0500

    Added Hugging Face to understand variations in user questions

--- LLM_QA/LLM_QA_model.py
+++ LLM_QA/LLM_QA_model.py
@@ -1,29 +1,45 @@
 import json
 import string
+from sentence_transformers import SentenceTransformer, util
 
 #Loading Q&A data
 with open("LLM_QA/Sample_QA.json", "r") as file:
     qa_data = json.load(file)
 
+#Get model
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+#Create normalization function
+def normalize(text):
+     return text.lower().translate(str.maketrans("","", string.punctuation)).strip()
+
+#Make embeddings of JSON Qs
+questions = list(qa_data.keys())
+normalized_qs = [normalize(q) for q in questions]
+q_embeddings = model.encode(normalized_qs, convert_to_tensor=True)
+
 #Need to define a function that correctly retrieves answer for a specified function
-def get_answer(question):
-    #Need to normalize user input so that it ignores capitalization and punctuation
-    normalized_input = question.lower().translate(str.maketrans("","", string.punctuation))
-    for q, a in qa_data.items():
-        #Have to normalize our Q
-        normalized_q = q.lower().translate(str.maketrans("","", string.punctuation)) #.lower to convert both to lower case and increase chances of matching
-        if normalized_input == normalized_q:
-            return a
-    return "I don't know, please ask a staff member."
-    
-#In the future, the robot will have a US sensor that triggers the response when user walks in range
+def get_answer(user_input):
+    norm_input = normalize(user_input)
+    input_embedding = model.encode(norm_input, convert_to_tensor=True)
+
+    #Generate similarities between pre-trained and user input
+    similarities = util.pytorch_cos_sim(input_embedding, q_embeddings)
+
+    #Find the best similarity in embeddings
+    best_match = similarities.argmax()
+    best_answer = questions[best_match]
+
+    return qa_data[best_answer]
+
+#Main Loop
 print("Hi, I'm a Katbot who can help answer any questions you have about the space. Do you have any questions?")
 
 while True:
-    question = input("You: ")
-    if question.lower() == "no":
+    user_input = input("You: ")
+    if user_input.lower() == "no":
         print("Ok, please let me know if you do!")
         break
 #Following runs when 'False' aka user has a Q
-    answer = get_answer(question)
+    answer = get_answer(user_input)
     print("Katbot:", answer)
